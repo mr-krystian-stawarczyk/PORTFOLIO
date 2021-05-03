37963ed04e16c02afc854096cae32da4e7f880d4
Krystian S <mr.krystian.stawarczyk@gmail.com>
2021-05-03 21:17:31 +0200

    Header Complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Particles from "react-particles-js";
 import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 
 function App() {
-	return <Navbar />;
+	return (
+		<>
+			<Particles
+				className="particles-canvas"
+				params={{
+					particles: {
+						number: {
+							value: 20,
+							density: {
+								enable: true,
+								value_area: 900,
+							},
+						},
+						shape: {
+							type: "circle",
+							stroke: {
+								width: 6,
+								color: "#f9ab00",
+								opacity: 0.7,
+							},
+						},
+					},
+				}}
+			/>
+			<Navbar />
+			<Header />
+		</>
+	);
 }
 
 export default App;
