83ab20c35bca71d1143d4b78cca4f6e76c09372c
Krystian Stawarczyk <72742707+mr-krystian-stawarczyk@users.noreply.github.com>
2021-05-21 17:54:23 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import App from "./src/App";
 
 ReactDOM.render(
-	<React.StrictMode>
+	<BrowserRouter>
 		<App />
-	</React.StrictMode>,
+	</BrowserRouter>,
 	document.getElementById("root")
 );
