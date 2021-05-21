ddd1b63442abf0c0831d0a1e1c8a894fce82ab7a
Krystian Stawarczyk <72742707+mr-krystian-stawarczyk@users.noreply.github.com>
2021-05-21 18:03:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import App from "./App";
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App path="./src/App.js"/>
+		<App exact="./src/App.js"/>
 	</React.StrictMode>,
 	document.getElementById("root")
 );
