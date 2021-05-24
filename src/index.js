48b38e1fad23703b12c8347caa45d69a517d8675
Krystian S <mr.krystian.stawarczyk@gmail.com>
2021-05-24 19:30:02 +0200

    zmiana 22

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-
+import BrowserRouter from "react-router-dom/BrowserRouter";
 ReactDOM.render(
-	<React.StrictMode>
+	<BrowserRouter basename={process.env.PUBLIC_URL}>
 		<App />
-	</React.StrictMode>,
+	</BrowserRouter>,
 	document.getElementById("root")
 );
